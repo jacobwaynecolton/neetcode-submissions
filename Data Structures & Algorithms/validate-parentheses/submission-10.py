f702e713f48c2e55c5886a88cb7d24025e51681e
jacobwaynecolton <jacob.wayne.colton@gmail.com>
2026-05-27 14:08:29 -0400

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Starting over with LIFO, but this time using dictionary,
+        # because the dictionary only needs to have 3 elements
+        valid_pairs = {'(' :')', '[':']', '{':'}'}
+        stack = []
+
+        for character in s:
+            if character in valid_pairs:
+                stack.append(character)
+            elif not stack:
+                return False
+            elif character != valid_pairs[stack.pop()]:
+                return False
+        return not stack
+        
