35aefc01d9c368046b30cd81fee253343f20b0ec
jacobwaynecolton <jacob.wayne.colton@gmail.com>
2026-06-02 13:44:36 -0400

    Add: binary-tree-diameter - submission-10

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-10.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
+        # Creating a nested recursive function, as we must 
+        # maintain track of what the current longest path is 
+
+        self.longest_path = 0      
+
+        def dfs(root):
+
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            current_longest_path = left + right
+
+            if current_longest_path > self.longest_path:
+                self.longest_path = current_longest_path
+            return 1 + max(left,right)
+        
+        dfs(root)
+        return self.longest_path
+        
