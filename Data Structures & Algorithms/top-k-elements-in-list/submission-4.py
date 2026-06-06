792c505961a6b7bf9b0137f6113b87c805ba1d39
jacobwaynecolton <jacob.wayne.colton@gmail.com>
2026-06-05 21:36:10 -0400

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Using a dictionary to store the counts for each number 
+        # Using bucket sort to solve this problem
+        # Creating the buckets
+        buckets = [[] for x in range(len(nums) + 1)]
+        # Creating the dictionary to keep track of the counts for each
+        # number in nums
+        freq_nums = {}
+        for num in nums:
+            # If the number doesn't already exist in nums, create it 
+            # at zero. Otherwise, increment
+            freq_nums[num] = freq_nums.get(num, 0) + 1
+        
+        # Now, iterating through the created dictionary,
+        # inputting each element into their respective buckets
+        for freq in freq_nums:
+            buckets[freq_nums[freq]].append(freq)
+        # Creating the final list to return and tracking current k
+        k_nums = []
+        counter = k
+        
+        # Iterating backwards through the buckets, to give the k largest
+        for bucket in reversed(buckets):
+            if counter == 0:
+                break
+            for num in bucket: 
+                k_nums.append(num)
+                counter -= 1
+        return k_nums
