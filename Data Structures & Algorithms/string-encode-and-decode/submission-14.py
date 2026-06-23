3fd53cada22e3df84251395bc24776dadbb30087
jacobwaynecolton <jacob.wayne.colton@gmail.com>
2026-06-23 09:31:27 -0400

    Add: string-encode-and-decode - submission-14

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-14.py
@@ -0,0 +1,39 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # The final string
+        final_str = "" 
+
+        # Iterating through each string in the list
+        for string in strs:
+            final_str += "x" + str(len(string))
+        final_str = str(len(final_str)) + final_str
+        final_str += "x" + ''.join(strs)
+
+        return final_str
+
+    def decode(self, s: str) -> List[str]:
+
+        print(s)
+        # First removing the initial stats
+        initial_stats_len = 0
+        for char in s:
+            if char != 'x':
+                initial_stats_len += 1
+            else:
+                break
+
+        initial_stats = int(s[:initial_stats_len])
+        strings = []
+        # Breaking up initial stats per item 
+        string_lengths = s[initial_stats_len+1:initial_stats_len +  initial_stats].split('x')
+        string_values = s[initial_stats + 1 + initial_stats_len:]
+        print(initial_stats)
+        print(string_lengths)
+        print(string_values)
+        if initial_stats == 0:
+            return []
+        for length in string_lengths:
+            strings.append(string_values[:int(length)])
+            string_values = string_values[int(length):]
+        return strings
