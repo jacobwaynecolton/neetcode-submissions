1a40b876b8e997c7283876399dba5306e030d886
Jacob Colton <jacob.wayne.colton@gmail.com>
2026-09-03 19:46:28 -0400

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.py
@@ -0,0 +1,29 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # Creating a sorted stack of tuples containing the speed and position for 
+        # each car, sorted by position in ascending order
+        car_stack = []
+
+        for i in range(len(position)):
+            # Calculating the time to target
+            target_time = (target - position[i])/speed[i]
+            car_stack.append((position[i],target_time))
+
+        # Sorting the car stack by position, then arrival time for tie-breaking
+        car_stack.sort() 
+    
+        # Keep track of the current current fleet max time (fleets in front of the current car)
+        fleet_max = 0
+
+        # Keep track of the current fleet count
+        fleet_count = 0
+        for i in range(len(car_stack)):
+            cur_car = car_stack.pop() 
+            
+            if cur_car[1] > fleet_max:
+                fleet_max = cur_car[1]
+                fleet_count += 1
+            
+        return fleet_count
+        
+
